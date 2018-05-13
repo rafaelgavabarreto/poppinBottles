c5d269f0e83fe69317f526bfb91c7c01a5047312
Rafael Barreto <rafaelgavabarreto@gmail.com>
2018-05-13 22:01:24 +0000

    Commit

--- poppinBottles.js
+++ poppinBottles.js
@@ -4,7 +4,9 @@ function poppinBottles() {
   var bottles = 0;
   var maxBottles = 0;
   var caps = 0;
-  pops = 0
+  var maxCaps = 0;
+  var pops = 0;
+  var maxPops = 0;
 
   do {
     while (value > 1) {
@@ -13,22 +15,28 @@ function poppinBottles() {
     }
     while (bottles > 1) {
       pops += 1;
+      maxBottles += 1;
       bottles -= 2;
     }
     while (caps > 3) {
       pops += 1;
+      maxCaps += 1;
       caps -= 4;
     }
 
     caps += pops;
     bottles += pops;
 
-    maxBottles += pops;
+    maxPops += pops;
+
     pops = 0;
 
   } while ((caps > 3) || (bottles > 1) || (pops > 0));
 
-return maxBottles;
+console.log('Total Bottles:',maxPops);
+console.log('Remaining Bottles:',bottles);
+console.log('Remaining Caps:',caps);
+console.log('Total Earned:\n'+'Bottles:',maxBottles,'\nCaps:',maxCaps);
 
 }
-console.log(poppinBottles());
+poppinBottles();
